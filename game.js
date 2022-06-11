bbecdcafd6c7fceda11af99933e5425a717ad44e
Laksh <raftaarmalhotra@gmail.com>
2022-06-11 09:01:40 +0530

    completed snake movement

--- game.js
+++ game.js
@@ -1,3 +1,28 @@
+import { update as updateSnake, draw as drawSnake, SNAKE_SPEED } from './snake.js'
+
+let lastRenderTime = 0
+const gameBoard = document.getElementById('game-board')
+
 function main(currenTime) {
     window.requestAnimationFrame(main)
+    const secondsSinceLastRender = (currenTime - lastRenderTime) / 1000
+    if (secondsSinceLastRender < 1 / SNAKE_SPEED) return
+
+
+    console.log('Render')
+    lastRenderTime = currenTime
+
+    update()
+    draw()
+}
+
+window.requestAnimationFrame(main)
+
+function update() {
+    updateSnake()
+}
+
+function draw() {
+    gameBoard.innerHTML = ''
+    drawSnake(gameBoard)
 }
